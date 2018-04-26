5d762f47a82acc214b1541d0a6ffd078ea8abb99
naorj <naorjoker@gmail.com>
2018-04-26 15:41:50 +0300

    upload main

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+#include "CircularInt.hpp"
+
+int main() {
+	CircularInt hour (1, 12);                 // <hour is an integer between 1 and 12, like an hour on the clock>
+	cout << hour << endl;                     // 1
+	hour += 4;  cout << hour << endl;         // 5
+	(hour += 2)++;  cout << hour << endl;     // 8
+	hour += 18;   cout << hour << endl;       // 2   (18 hours after 8)
+	cout << -hour << endl;                   // 10  (2 hours before midnight)
+	hour = 1 - hour;	cout << hour << endl;    // 11  (2 hours before 1)
+	cout << hour+hour << endl;                // 10 (11 hours after 11)
+	hour *= 2;   cout << hour << endl;        // 10 (11*2 = 11+11)
+	cout << hour/2 << endl;                   // TWO OPTIONS: 11 (since 11*2=10) or 5 (since 5*2=10 too).
+	cout << "-------------------------------" << endl;
+	try {
+		//cout << hour/11<<endl;
+		cout << hour/3;
+	} catch (const string& message) {
+		cout << message << endl;     // "There is no number x in {1,12} such that x*3=10"
+	}
+
+	/*cout << endl;			// operator '==' check
+	cout<<hour<<endl;
+	hour=hour-8;
+	cout<<hour<<endl;
+	CircularInt hour2 (1,12);
+	cout << hour2<< endl;
+	if(hour==hour2)
+		cout<<"hey";
+	else
+		cout<<"g";*/
+}
